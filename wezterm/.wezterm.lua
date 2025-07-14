c3c41129429a52edfcfa56db426fb7ca131b407b
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2025-07-14 13:15:17 +0530

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -37,6 +37,7 @@ config.hide_mouse_cursor_when_typing = false
 
 -- Remove all padding
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.line_height = 0.9
 
 -- Font configuration
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
